1981f60b43be3534bee8061688ba53047d69717a
Angshuman Neogi <94470243+angshuneogi@users.noreply.github.com>
2022-09-12 20:07:21 +0530

    Add files via upload

---
+++ genetic algorithm files/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+n=20            # String Size of each variable
+N=70            # Population Size of Solution Matrix
+Pc = 1        # Crossover Probability
+Pm = 0.05        # Mutation Probability
+Gen=50          # Number Of Generations
